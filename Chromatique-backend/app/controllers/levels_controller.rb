7cb2fe2c2d1231414bc656513028611b1f7a048a
Ben Yellin <byellindc@gmail.com>
2019-03-04 12:24:53 -0500

    add level endpoint

--- Chromatique-backend/app/controllers/levels_controller.rb
+++ Chromatique-backend/app/controllers/levels_controller.rb
@@ -1,2 +1,17 @@
 class LevelsController < ApplicationController
+  before_action :get_level, only: %i(show)
+
+  def index
+    render json: Level.all, status: 200
+  end
+
+  def show
+    render json: @level, status: 200
+  end
+
+  private 
+
+  def get_level
+    @level = Level.find(params[:id])
+  end
 end
