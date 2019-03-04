bdcd76b1c1a4e49a75d713de874f64fb34ff75ef
github username <github email address>
2019-03-04 12:32:31 -0500

    comments

--- Chromatique-backend/config/routes.rb
+++ Chromatique-backend/config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  # /users/:id    => user show
+  # /users/:id/levels    => level index belonging to a user
+  # /users/:id/levels/:id    => level show
+  # /levels    => published levels belonging to any user
+  # /levels/:id /users    => user index of those that completed that level
   resources :levels
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
