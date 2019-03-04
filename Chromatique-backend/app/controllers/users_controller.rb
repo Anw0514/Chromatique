f13ed8f748c1f69b48776f14065324c3035d805e
Ben Yellin <byellindc@gmail.com>
2019-03-04 12:16:15 -0500

    add user show endpoint

--- Chromatique-backend/app/controllers/users_controller.rb
+++ Chromatique-backend/app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+  before_action :get_user, only: %i(show)
+
+  def show
+    render json: @user, status: 200
+  end
+
+  private 
+
+  def get_user
+    @user = User.find(params[:id])
+  end
 end
