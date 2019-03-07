c28d648b8f3bd0a9fae9af2d5c5d01ba51718ce8
github username <github email address>
2019-03-07 17:17:39 -0500

    new levels and delete buttons

--- Chromatique-backend/app/controllers/levels_controller.rb
+++ Chromatique-backend/app/controllers/levels_controller.rb
@@ -9,9 +9,21 @@ class LevelsController < ApplicationController
     render json: @level, status: 200
   end
 
+  def create
+    Level.create!(level_params)
+  end
+
+  def destroy
+    Level.destroy(params[:id])
+  end
+
   private 
 
   def get_level
     @level = Level.find(params[:id])
+  end
+
+  def level_params
+    params.require(:level).permit(:name, :top_right, :bottom_right, :bottom_left, :top_left, :grid_size, :published, :user_id, :user, :creator, :creator_id)
   end
 end
