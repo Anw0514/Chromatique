427585eec02ccc073a78c08ad5babf4fba0c44ef
Ben Yellin <byellindc@gmail.com>
2019-03-07 18:43:52 -0500

    fix routes

--- Chromatique-backend/config/routes.rb
+++ Chromatique-backend/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # /levels    => published levels belonging to any user
   # /levels/:id/users    => user index of those that completed that level
-  resources :levels, only: %i(index show create destroy)
+  resources :levels, only: %i(index show create destroy) do
     resources :users, only: :index
   end
 
